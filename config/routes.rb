4533ece2bdbd63f85ff860141bbb75e0bf3ac98f
Spencer <lesp@seas.upenn.edu>
2012-01-14 05:13:34 -0500

    Fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
   get "pages/home"
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-
   root :to => 'pages#home'
-
   match '/signup', :to => 'users#new'
-  match '/about', :to => 'pages#about'
-  match '/help', :to => 'pages#help'
-
-  root :to => 'pages#home'
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -63,7 +54,6 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
