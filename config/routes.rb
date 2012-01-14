ed71fe46c7d06936e2c8330eafae5a0fdb2ba6f9
wmcd <wmcd@seas.upenn.edu>
2012-01-14 05:12:13 -0500

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
-  get "pages/home"
   match '/contact', :to => 'pages#contact'
-  match '/about', :to => 'pages#about'
-  match '/help', :to => 'pages#help'
-
-  root :to => 'pages#home'
-
   match '/signup', :to => 'users#new'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
