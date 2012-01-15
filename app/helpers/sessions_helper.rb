b7ecfa1bef8a7841cbc6c327a98a70f62be4da25
wmcd <wmcd@seas.upenn.edu>
2012-01-14 22:26:31 -0500

    hgdai~uhgsduhbasui

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,10 +2,10 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    self.current_user = user
+    @current_user = user
   end
 
-  def current_user=(user)
+  def current_user
     @current_user = user ||= user_from_remember_token
   end
 
@@ -15,7 +15,7 @@ module SessionsHelper
 
   def sign_out
     cookies.delete(:remember_token)
-    self.current_user = nil
+    @current_user = nil
   end
 
   private
