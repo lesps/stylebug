fcfbd5e8d9c7a81ef3845f073845fd334706fd35
wmcd <wmcd@seas.upenn.edu>
2012-01-14 19:46:54 -0500

    Bunch of site work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
-    @user = User.new
+    @user = User.new(params[:user])
     @title = "Sign up"
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Stylbug!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 end
