3c7cd1fcf54ba3f3606c453cd34e9e6151379b56
wmcd <wmcd@seas.upenn.edu>
2012-01-14 22:33:14 -0500

    ihgaishgs

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,12 +5,12 @@ module SessionsHelper
     @current_user = user
   end
 
-  def current_user
+  def get_current_user
     @current_user = user ||= user_from_remember_token
   end
 
   def signed_in?
-    !current_user.nil?
+    !get_current_user.nil?
   end
 
   def sign_out
